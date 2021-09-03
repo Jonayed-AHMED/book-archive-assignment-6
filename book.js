b75cf65c4939b1b509eae2295667a5691d82ad29
Jonayed-AHMED <rifatctgbd16@gmail.com>
2021-09-03 15:10:58 +0600

    second commit

--- book.js
+++ book.js
@@ -1,43 +1,60 @@
-const searchBook = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // console.log(searchText);
+document.getElementById('showingResults').style.display = 'none';
+document.getElementById('errorResults').style.display = 'none';
+const searchBooks = () => {
+	const searchField = document.getElementById('search-field');
+	const searchFieldTxt = searchField.value;
+	document.getElementById('showingResults').style.display = 'none';
+	document.getElementById('errorResults').style.display = 'none';
 
-    // clear data
-    
-    searchField.value = '';
+	//clear value
+	searchField.value = '';
 
-    // load data
+	fetch(`https://openlibrary.org/search.json?q=${searchFieldTxt}`)
+		.then((res) => res.json())
+		.then((data) => {
+			diplayBooks(data.docs, data.numFound);
+		});
+};
+const diplayBooks = (books, num) => {
+	// console.log();
+	if (books.length > 0) {
+		const bookResults = document.getElementById('books-results');
+		bookResults.innerHTML = '';
+		document.getElementById('showingResults').style.display = 'block';
 
-    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.docs));
+		const totalResults = document.getElementById('total-results');
 
-}
-const displaySearchResult = docs => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    if (docs.length === 0) {
-        alert("No result found");
-    }
-    docs.forEach(doc => {
-        console.log(doc);
-        const div = document.createElement('div');
-        div.classList.add('cols');
-        div.innerHTML = `
-        <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title fs-4 text-dark">${doc.title}</h5>
-                    <h2 class="card-text fs-6">Auther : ${doc.author_name}</h2>
-                    <h2 class="card-text fs-6">Publisher : ${doc.publisher}</h2>
-                    <h2 class="card-text fs-6">First Publish Year : ${doc.first_publish_year}</h2>
-                    <p class="card-text"></p>
-                </div>
-            </div>
-        `;
-        searchResult.appendChild(div);
+		totalResults.innerText = `${num}`;
 
-    })
-}
+		const myBooks = books.slice(0, 20);
+		// console.log(myBooks);
+		// console.log(books);
+		// console.log(books);
+		myBooks.forEach((book) => {
+			// console.log(book.docs.length);
 
+			const div = document.createElement('div');
+			div.classList.add('col');
+			div.innerHTML = `<div  class="card">
+						<img
+							src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
+							class="img-fluid"
+						/>
+					<div class="card-body">
+						<h5 class="card-title">Book title: ${book.title}</h5>
+						<p class="card-text">
+							Author name: ${book.author_name[0]}
+						</p>
+						<p class="card-text">
+						 First published: ${book.first_publish_year}
+						</p>
+					</div>
+				</div>`;
+			bookResults.appendChild(div);
+		});
+	} else {
+		document.getElementById('errorResults').style.display = 'block';
+		document.getElementById('showingResults').style.display = 'none';
+		bookResults.innerHTML = '';
+	}
+};
